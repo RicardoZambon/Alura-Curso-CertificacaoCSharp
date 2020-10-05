9283e64a1c2e86992e546bef398da713ccb1065c
Ricardo Zambon <ricardo.zambon@smartm.com>
2020-10-05 10:33:18 -0300

    Part 6 complete.

---
+++ CertificacaoCSharp/Part6/JSON/MovieStore.cs
@@ -0,0 +1,40 @@
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+
+namespace Zambon.Alura.CertificacaoCSharp.Part6.JSON
+{
+    [JsonObject(nameof(LojaDeFilmes))]
+    public class MovieStore
+    {
+        [JsonProperty(nameof(LojaDeFilmes.Diretores))]
+        public List<Director> Directors = new List<Director>();
+        [JsonProperty(nameof(LojaDeFilmes.Filmes))]
+        public List<Movie> Movies = new List<Movie>();
+        public static MovieStore AddMovie(Movie movie)
+        {
+            MovieStore store = new MovieStore();
+            // ...
+            return store;
+        }
+    }
+
+    [JsonObject(nameof(Diretor))]
+    public class Director
+    {
+        [JsonProperty(nameof(Diretor.Nome))]
+        public string Name { get; set; }
+        [JsonIgnore]
+        public int NumberOfMovies;
+    }
+
+    [JsonObject(nameof(Filme))]
+    public class Movie
+    {
+        [JsonProperty(nameof(Filme.Diretor))]
+        public Director Director { get; set; }
+        [JsonProperty(nameof(Filme.Titulo))]
+        public string Title { get; set; }
+        [JsonProperty(nameof(Filme.Ano))]
+        public string Year { get; set; }
+    }
+}
