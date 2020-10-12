3aa85fa7e2c1c7f5d3e383648a599c0bc8bb87c6
Ricardo Zambon <ricardo.zambon@hotmail.com>
2020-10-12 14:10:17 -0300

    Finish part 06.

---
+++ CertificacaoCSharp/Part6/02-Collections/CustomCollections/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace Zambon.Alura.CertificacaoCSharp.Collections.CustomCollections
+{
+    class Program
+    {
+        static PlacasDeCarro placas = new PlacasDeCarro();
+
+        static void Main(string[] args)
+        {
+            placas.Add("FND-7714");
+            placas.Add("ABC-1234");
+            placas.Add("XYZ-9987");
+            placas.Add("123-ABCD");
+
+            foreach (var placa in placas)
+            {
+                Console.WriteLine(placa);
+            }
+
+            //PROBLEMA: CRIAR UMA COLEÇÃO DE PLACAS DE CARRO VÁLIDAS
+            //SOLUÇÃO: CRIAR UMA COLEÇÃO PERSONALIZADA
+        }
+
+
+    }
+
+    class PlacasDeCarro : ICollection<string>
+    {
+        private List<string> lista = new List<string>();
+
+
+        public void Add(string item)
+        {
+            if (!EhPlacaValida(item))
+            {
+                throw new ArgumentException($"Placa não é válida: {item}");
+            }
+            lista.Add(item);
+        }
+
+        public void Clear() => lista.Clear();
+
+        public bool Contains(string item) => lista.Contains(item);
+
+        public void CopyTo(string[] array, int arrayIndex) => lista.CopyTo(array, arrayIndex);
+
+        public int Count => lista.Count;
+
+        public IEnumerator<string> GetEnumerator() => lista.GetEnumerator();
+
+        public bool IsReadOnly => false;
+
+        public bool Remove(string item) => lista.Remove(item);
+
+        IEnumerator IEnumerable.GetEnumerator() => lista.GetEnumerator();
+
+        static bool EhPlacaValida(string value)
+            => new Regex(@"^[A-Z]{3}\-\d{4}$").IsMatch(value);
+    }
+}
---
+++ CertificacaoCSharp/Part6/02-Collections/Queues/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Zambon.Alura.CertificacaoCSharp.Collections.Queues
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //COLEÇÃO: FILA = QUEUE
+            //Regra: primeiro que entra é o primeiro que sai
+
+            //TAREFA: Implementar uma fila de pedágio
+
+            var pedagio = new Pedagio();
+
+            pedagio.Enfileirar("Van");
+            pedagio.Enfileirar("Kombi");
+            pedagio.Enfileirar("Guincho");
+            pedagio.Enfileirar("Pickup");
+
+            pedagio.Desenfileirar();
+            pedagio.Desenfileirar();
+            pedagio.Desenfileirar();
+            pedagio.Desenfileirar();
+        }
+    }
+
+    class Pedagio
+    {
+        Queue<string> fila = new Queue<string>();
+
+        public void Enfileirar(string veiculo)
+        {
+            fila.Enqueue(veiculo);
+
+            Console.WriteLine("Entrou na fila: " + veiculo);
+            Imprimir();
+        }
+
+        private void Imprimir()
+        {
+            Console.WriteLine("VEÍCULOS NA FILA:");
+            foreach (var v in fila)
+            {
+                Console.WriteLine(v);
+            }
+            Console.WriteLine("---------------------------------");
+        }
+
+        public void Desenfileirar()
+        {
+            if (fila.Count == 0)
+            {
+                Console.WriteLine("A fila já está vazia!");
+                return;
+            }
+
+            var veiculo = fila.Dequeue();
+            Console.WriteLine("Saiu da fila: " + veiculo);
+
+            if (fila.Count == 0)
+            {
+                Console.WriteLine("A fila já está vazia!");
+                return;
+            }
+
+            var proximo = fila.Peek();
+            Console.WriteLine("O próximo da fila é: " + proximo);
+            Imprimir();
+        }
+    }
+}
